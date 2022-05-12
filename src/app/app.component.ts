27e3cc1914835523020e42f46d3123a800688baa
RaduTheMan <radu.damian07@gmail.com>
2022-05-12 21:55:55 +0300

    added bootstrap and main navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+const maxLengthAuthorName = 20;
+const maxLengthExcedeedMessage = `Max length of ${maxLengthAuthorName} characters excedeed!`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Social Media';
+  title = 'Writify';
+  searchFormGroup: FormGroup;
+
+  constructor(private readonly snackBar: MatSnackBar) {
+    this.searchFormGroup = new FormGroup({
+      authorName: new FormControl(null, Validators.maxLength(maxLengthAuthorName))
+    });
+  }
+
+  inputValidator(event: any) {
+    const errors = this.searchFormGroup.get('authorName')?.errors;
+    if(errors !== null && errors !== undefined) {
+      if(Object.keys(errors as ValidationErrors).includes('maxlength')){
+        const value = event.target.value as String;
+        event.target.value = value.slice(0, value.length - 1);
+        this.searchFormGroup.get('authorName')?.setValue(event.target.value);
+      }
+    }
+  }
+
+  onEnter() {
+    if(this.searchFormGroup.get('authorName')?.valid){
+      console.log('valid');
+    }
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule, } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const components = [
+  MatButtonModule, 
+  MatToolbarModule, 
+  MatFormFieldModule,
+  MatInputModule, 
+  MatSelectModule,
+  MatSnackBarModule
+];
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule]
+  imports: [...components],
+  exports: [...components]
 })
 export class MaterialModule { }
